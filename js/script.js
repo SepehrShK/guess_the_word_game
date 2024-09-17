ec093a7bae827350c0a80d46dd38062d24a3fcb3
Sepehr Shayan Kia <sepehrshayankia@gmail.com>
2024-09-17 14:25:03 +0330

    making some changes to the last commit

--- js/script.js
+++ js/script.js
@@ -36,41 +36,52 @@ let shuffledWord = shuffle(randomWord);
 console.log(shuffledWord);
 
 
-const scramblle_word = document.querySelector('.scramblle_word p')
-scramblle_word.innerHTML = ""
+const scramblleWord = document.querySelector('.scramblle_word p')
+scramblleWord.innerHTML = ""
 
 const input = document.querySelector('.input')
 
 for (let i = 0; i < shuffledWord.length; i++) {
-    scramblle_word.innerHTML += ` ${shuffledWord[i]}`
+    scramblleWord.innerHTML += ` ${shuffledWord[i]}`
 
     const input_box = document.createElement('input')
     input_box.className = "input_box";
     input_box.setAttribute('type', 'text')
     input_box.setAttribute('maxlength', '1')
     input_box.setAttribute('title', `Letter ${i+1}`)
+    input_box.disabled = true;
     input.appendChild(input_box);
 }
 
 
 const inputBoxes = document.querySelectorAll('.input_box');
 
-inputBoxes.forEach((box, index) => {
-  box.addEventListener('input', (event) => {
-    const currentBox = event.target;
-    const nextBox = inputBoxes[index + 1];
-
-    if (currentBox.value.length === 1 && nextBox) {
-      nextBox.focus();
-    }
-  });
-
-  box.addEventListener('keydown', (event) => {
-    if (event.key === 'Backspace' && box.value === '' && index > 0) {
-      inputBoxes[index - 1].focus();
-    }
-  });
+window.addEventListener('load', () => {
+    inputBoxes[0].disabled = false;
+    inputBoxes[0].focus();
 });
+
+inputBoxes.forEach((box, index) => {
+    box.addEventListener('input', (event) => {
+        const currentBox = event.target;
+        const nextBox = inputBoxes[index + 1];
+
+        if (currentBox.value.length === 1 && nextBox) {
+            nextBox.disabled = false;
+            nextBox.focus();
+            currentBox.disabled = true;
+        }
+    });
+
+    box.addEventListener('keydown', (event) => {
+        if (event.key === 'Backspace' && box.value === '' && index > 0) {
+            inputBoxes[index - 1].disabled = false;
+            inputBoxes[index - 1].focus();
+            inputBoxes[index].disabled = true;
+        }
+    });
+});
+
 
 
 
